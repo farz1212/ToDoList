d2cad2e1296731da166127ae7ea7ff3ab372f69a
farz1212 <farzaad1212@gmail.com>
2024-02-15 12:14:08 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Farzaad Goiporia on 2024-02-15.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
